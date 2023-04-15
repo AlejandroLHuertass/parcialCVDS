c85ad70789443fbe037dce6309f7e0ebde5808f0
AlejandroLHuertass <88836525+AlejandroLHuertass@users.noreply.github.com>
2023-04-15 09:53:30 -0500

    Delete main directory

--- main/java/com/lab/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lab.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lab.demo.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
